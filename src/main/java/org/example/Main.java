f80999c009ce9384bc40c0c6f83d88d9f68820e5
Tomasz-Zogala <tomasz.jakub.zogala@gmail.com>
2023-05-17 20:09:23 +0200

    Basic User Interface Idea

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,46 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("First Commit!");
-        System.out.println("First Commit!");
-        System.out.println("First Commit!");
-        System.out.println("First Commit!");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Choose activity");
+            System.out.println("1. Show products");
+            System.out.println("2. Add product");
+            System.out.println("3. Remove product");
+            System.out.println("4. Show 3 possible ideas for breakfast");
+            System.out.println("5. Show 3 possible ideas for dinner");
+            System.out.println("6. Get healthy products recommendation");
+            System.out.println("7. Quit");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1 -> {
+                    System.out.println("Products");
+                }
+                case 2 -> {
+                    System.out.println("Which product do you want to add");
+                }
+                case 3 -> {
+                    System.out.println("Which product do you want to remove");
+                }
+                case 4 -> {
+                    System.out.println("Breakfast ideas from ChatGPT");
+                }
+                case 5 -> {
+                    System.out.println("Dinner ideas from ChatGPT");
+                }
+                case 6-> {
+                    System.out.println("Healthy products recommended by ChatGPT");
+                }
+                case 7 -> {
+                    System.out.println("Bon Appetit");
+                }
+            }
+        }
     }
 }
